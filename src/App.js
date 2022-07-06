80cce5d05fb7f25afe47565c40915be830a9bb0e
Christopher Alphonse <christopheralphonse96.com>
2022-07-05 23:53:28 -0400

    Framer x tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-function App() {
+const App = () => {
+  const [mousePos, setMousePos] = useState({
+    x: 0,
+    y: 0,
+  });
+  // console.log(mousePos);
+  const [cursorVar, setCursorVar] = useState("default");
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePos({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const variants = {
+    default: {
+      x: mousePos.x - 16,
+      y: mousePos.y - 16,
+      backgroundColor: "#fde047",
+      mixBlendMode: "difference",
+    },
+    text: {
+      height: 150,
+      width: 150,
+    },
+  };
+  const textEnter = () => setCursorVar("text");
+  const textLeave = () => setCursorVar("default");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-[100vh] bg-yellow-300">
+      <h1
+        onMouseEnter={textEnter}
+        onMouseLeave={textLeave}
+        className="font-bold md:text-[5rem] xl:text-[10rem] pointer-events-none"
+      >
+        Hello Tailwind x Motion
+      </h1>
+      <motion.div
+        variants={variants}
+        animate={cursorVar}
+        className="bg-[#111] h-[32px] w-[32px] rounded-full fixed top-0 left-0 "
+      ></motion.div>
     </div>
   );
-}
+};
 
 export default App;
